8f6fcd7f70aeaf130f32a84532c15beb3674101c
David Keim <dmk252@cornell.edu>
2023-04-07 16:42:05 -0400

    fixed issue with loading header

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -18,7 +18,8 @@ export const putDb = async (content) => {
   const jateDb = await openDB('jate', 1);
   const tx = jateDb.transaction('jate', 'readwrite');
   const store = tx.objectStore('jate');
-  const request = store.add(conten);
+  console.log("content:", content);
+  const request = store.add({ jate: content });
   const result = await request;
   console.log('Data saved to the database', result);
 };
@@ -31,8 +32,15 @@ export const getDb = async () => {
   const store = tx.objectStore('jate');
   const request = store.getAll();
   const result = await request;
-  console.log('result.value', result);
-  return result;
+  let returnResult;
+  try {
+    returnResult = result[result.length - 1].jate;
+  }
+  catch {
+    returnResult = null;
+  }
+  console.log('result.value', returnResult);
+  return returnResult;
 };
 
 initdb();
